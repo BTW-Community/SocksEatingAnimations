4c14bc96a5717fa68866ebef877e40de44211f55
socklessthing <sockthinggaming@gmail.com>
2021-10-28 07:20:21 +0200

    Made Changes to avoid too many files

--- minecraft/net/minecraft/src/SEADefs.java
+++ minecraft/net/minecraft/src/SEADefs.java
@@ -1,13 +1,14 @@
 package net.minecraft.src;
 
 public class SEADefs {
-
+	
+	static String[] appleTextures = new String[] {"apple", "apple_1" , "apple_2", "apple_3", "apple_4"};
+	static String[] breadTextures = new String[] {"bread", "bread_1" , "bread_2", "bread_3", "bread_4"};
+	
 	public static void addDefinitions() {
 
-		Item.appleRed = Item.replaceItem(Item.appleRed.itemID, SEAItemAppleRed.class, SocksEatingAnimationsAddon.instance);
-		
-		//public static Item bowlSoup = ( new FCItemMushroomSoup( 26, 3 ) ).setUnlocalizedName( "mushroomStew" );
-		//public static Item bread = ( new ItemFood( 41, 3, 0.25F, false ) ).setUnlocalizedName( "bread" );
+		Item.appleRed = Item.replaceItem(Item.appleRed.itemID, SEAItemFood.class, SocksEatingAnimationsAddon.instance, 1, 0F, false, "apple", appleTextures).SetFilterableProperties( Item.m_iFilterable_Small );
+		Item.bread = Item.replaceItem(Item.bread.itemID, SEAItemFood.class, SocksEatingAnimationsAddon.instance, 3, 0.25F, false, "bread", breadTextures);
 	}
 
 }
--- minecraft/net/minecraft/src/SEAItemFood.java
+++ minecraft/net/minecraft/src/SEAItemFood.java
@@ -3,19 +3,22 @@ package net.minecraft.src;
 public class SEAItemFood extends FCItemFood {
 
 	static public final int m_iProgressTimeInterval = 5;
+	public String[] eatingProgressArray;
 
-	public SEAItemFood( int iItemID, int iHungerHealed, float fSaturationModifier, boolean bWolfMeat, String sItemName )
+	public SEAItemFood( int iItemID, int iHungerHealed, float fSaturationModifier, boolean bWolfMeat, String sItemName, String[] eatingTextures )
     {
         super( iItemID, iHungerHealed, fSaturationModifier, bWolfMeat, sItemName );
         setMaxDamage( 0 ); //Disable progress bar
-        setUnlocalizedName( sItemName );    
+        setUnlocalizedName( sItemName );
+        this.eatingProgressArray = eatingTextures;
     }
     
-    public SEAItemFood( int iItemID, int iHungerHealed, float fSaturationModifier, boolean bWolfMeat, String sItemName, boolean bZombiesConsume )
+    public SEAItemFood( int iItemID, int iHungerHealed, float fSaturationModifier, boolean bWolfMeat, String sItemName, boolean bZombiesConsume , String[] eatingTextures )
     {
         super( iItemID, iHungerHealed, fSaturationModifier, bWolfMeat, sItemName, bZombiesConsume );
         setMaxDamage( 0 ); //Disable progress bar
-        setUnlocalizedName( sItemName );    
+        setUnlocalizedName( sItemName );
+        this.eatingProgressArray = eatingTextures;
     } 
     
     @Override
@@ -62,4 +65,22 @@ public class SEAItemFood extends FCItemFood {
 		return super.onEaten(par1ItemStack, par2World, par3EntityPlayer);
 	}
 
+	private Icon[] iconArray; 
+	 
+	public void registerIcons(IconRegister par1IconRegister) 
+	{ 
+	    super.registerIcons(par1IconRegister); 
+	    this.iconArray = new Icon[eatingProgressArray.length]; 
+	 
+	    for (int var2 = 0; var2 < this.iconArray.length; ++var2) 
+	    { 
+	        this.iconArray[var2] = par1IconRegister.registerIcon(eatingProgressArray[var2]); 
+	    } 
+	} 
+	 
+	@Override 
+	public Icon getIconFromDamage(int par1) { 
+		return this.iconArray[par1]; 
+	} 
+	
 }
