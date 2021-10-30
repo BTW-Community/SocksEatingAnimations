eaca768fb67908496ee0a280bdc151ff14058f27
socklessthing <sockthinggaming@gmail.com>
2021-10-30 20:45:35 +0200

    Initial Commit

---
+++ minecraft/net/minecraft/src/SocksEatingAnimationsAddon.java
@@ -0,0 +1,40 @@
+package net.minecraft.src;
+
+import java.util.List;
+
+/**
+ * @author Sockthing (@socklessthing)
+ *
+ */
+public class SocksEatingAnimationsAddon extends AddonExt {
+	private static SocksEatingAnimationsAddon instance;
+
+	public SocksEatingAnimationsAddon() {
+		super("Sock's Eating Animations", "1.0.0", "SEA");
+	}
+
+	public static SocksEatingAnimationsAddon getInstance() {
+		if (instance == null) {
+			instance = new SocksEatingAnimationsAddon();
+		}
+
+		return instance;
+	}
+
+	@Override
+	public void Initialize() {
+		
+//		AddEntityMappings();
+//		ReplaceEntityMappings();
+//		AddEntityRenderer();	
+//		AddBiomeSpawn();
+//		SocksMobsDefs.addDefinitions(); //add blocks and items
+	}
+	
+	public String GetLanguageFilePrefix()
+	{
+		return "SEA";
+	}
+
+
+}
